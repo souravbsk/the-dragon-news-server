e510b91cd9e3d4f3b9518874a9ccc3429914c595
souravbsk <souravbsk01@gmail.com>
2023-04-29 01:23:50 +0600

    server site deploy with vercel config

--- index.js
+++ index.js
@@ -3,17 +3,49 @@ const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 
-const categories = require("./data/categories.json")
 app.use(cors())
+const categories = require("./data/categories.json")
+const news = require('./data/news.json');
 
 
 app.get("/",(req,res) => {
     res.send('hello world')
 })
 
+
+app.get("/news",(req,res) => {
+    res.send(news)
+})
+
+
+//find single news
+app.get("/news/:id",(req,res) => {
+    const id = req.params.id;
+    const singleNews = news.find(n => n._id == id);
+    res.send(singleNews)
+})
+
+//find news category
+app.get("/categories/:id",(req,res) => {
+    const id = parseInt(req.params.id);
+
+    if(id === 0){
+        res.send(news)
+    }
+    else{
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id);
+        res.send(categoryNews)
+
+    }
+
+})
+
 app.get("/categories",(req,res) => {
     res.send(categories)
 })
+
+
+
 //server run
 app.listen(port,() => {
    
